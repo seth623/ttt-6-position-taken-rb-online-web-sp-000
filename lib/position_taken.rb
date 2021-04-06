e6e6edd395fa30550291dc500877df1cb00732fc
colorful-state-34cc <seth623@gmail.com>
2021-04-06 22:31:43 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,5 +4,9 @@ def position_taken?(board, index)
     return false
   elsif board[index] != ""
     return false
+  elsif board[index] == " "
+    return true
+  elsif board[index] == ""
+    return true
   end
 end
