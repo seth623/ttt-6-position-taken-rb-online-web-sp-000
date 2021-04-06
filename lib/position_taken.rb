21d897bae4dbe7704d6cbf91feccd2d1957f519a
colorful-state-34cc <seth623@gmail.com>
2021-04-06 22:45:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,12 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != " "
-    return false
-  elsif board[index] != ""
-    return false
+  if board[index] == "X"
+    return true
+  elsif board[index] == "O"
+    return true
   elsif board[index] == " "
-    return true
+    return false
   elsif board[index] == ""
-    return true
+    return false
   end
 end
